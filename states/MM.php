61e8edd98edbc317ece4d5134adbb7545060c9e7
kaekoko <kekoko.mm@gamil.com>
2020-08-03 20:42:34 +0630

    first commit

---
+++ states/MM.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Myanmar states
+ *
+ * 
+ */
+
+global $states;
+
+$states['MM'] = array(
+						"YANGON" => "Yangon Division",
+      					"AYA"  => "Ayeyarwady Division",
+				        "BGE" => "Bago East Division",
+				        "BGW" => "Bago West Division",
+				        "CHIN" => "Chin State",
+				        "KACHIN" => "Kachin State",
+				        "KAYAH" => "Kayah State",
+				        "KAYIN" => "Kayin State",
+				        "MAGWAY" => "Magway Division",
+				        "MDY" => "Mandalay Division",
+				        "MON" => "Mon State",
+				        "RAKHINE" => "Rakhine State",
+				        "SAGAING" => "Sagaing Division",
+				        "SHAN_E" => "Shan State (East)",
+						"SHAN_N" => "Shan State (North)",
+						"SHAN_S" => "Shan State (South)",
+						"TANINTHARYI" => "Tanintharyi Division"
+						
+						
+				       
+				       );
+  return $states;
